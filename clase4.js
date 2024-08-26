1685dcf85f42a3111952b76797e88e558d7e5a63
JhosuaCarrascal <javyjhosuarojascarrascal@gmail.com>
2024-08-26 17:02:59 -0500

    clase-4

---
+++ clase4.js
@@ -0,0 +1,7 @@
+const calcularExpresion = ({ a, b, c }) => (a * b) + c;                                    
+    const resultado1 = {                        
+     a:10, b:2, c:5,                           
+    };
+const resultado = calcularExpresion(resultado1);
+
+console.log(resultado);
